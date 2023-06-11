0fbe7d77a1950bcf69ab690ec2104be58c3f167f
Chris Choi <mychoi314@gmail.com>
2023-06-11 18:40:14 -0400

    Added more stuff to try to figure out why there's an analytical disc

--- massive.py
+++ massive.py
@@ -7,8 +7,8 @@ from scipy.integrate import odeint
 H_inf = 1  # 1e14  # in gev, according to https://cds.cern.ch/record/1420368/files/207.pdf
 a_r = 1  # 1e-32 # preliminary value, will change later
 tau_r = 1/(a_r*H_inf)
-k = .1
-tau_m = 20*tau_r  # 5*tau_r tau_m will be before matter-rad equality, but after inflation ends https://arxiv.org/pdf/1808.02381.pdf
+k = 10
+tau_m = 1e10*tau_r  # 5*tau_r tau_m will be before matter-rad equality, but after inflation ends https://arxiv.org/pdf/1808.02381.pdf
 # 1e-30 # from Claude de Rham paper https://arxiv.org/pdf/1401.4173.pdf
 # m = 1*np.sqrt(2) - .1
 # m = 1*np.sqrt(2) + .00001
@@ -17,7 +17,9 @@ nu = np.sqrt(9/4 - m**2 / H_inf**2)
 N = 10000
 tau_init = -2000*tau_r
 # tau = np.linspace(tau_init, tau_m + tau_r, N)
-tau = np.linspace(tau_init, -tau_r, N)
+tau = np.logspace(tau_init, -tau_r, N)
+
+isReal = False
 
 
 def scale_fac(conf_time):
@@ -46,10 +48,6 @@ def M_derivs_homo(M, u):
     return [M[1], -(k**2 + (scale_fac(u)*mu(u))**2 - d_scale_fac_dz(u) / scale_fac(u)) * M[0]]
 
 
-def f_no_m(M, u):
-    return [M[1], -(k**2 - d_scale_fac_dz(u) / scale_fac(u)) * M[0]]
-
-
 fig, (ax1) = plt.subplots(1, figsize=(22, 14))
 
 tau_up_to_r = np.empty([0])
@@ -64,35 +62,51 @@ xx = (np.sqrt(-np.pi*xxx) / 2 * scipy.special.hankel1(nu, -k*xxx))[:2]
 xx = xx[1] - xx[0]
 xx /= xxx[1] - xxx[0]
 print(xx)
-tau_up_to_r = np.linspace(tau_init, -tau_r, N)
+tau_up_to_r = np.logspace(tau_init, -tau_r, N)
 
 
 # Get the homogeneous solution using scipy.integrate.odeint
-v_0 = np.sqrt(-np.pi*tau_up_to_r[0]) / 2 * \
-    scipy.special.hankel1(nu, -k*tau_up_to_r[0]).imag
-v_prime_0 = xx.imag
+if isReal:
+    v_0 = np.sqrt(-np.pi*tau_up_to_r[0]) / 2 * \
+        scipy.special.hankel1(nu, -k*tau_up_to_r[0])
+    v_prime_0 = xx
+else:
+    v_0 = np.sqrt(-np.pi*tau_up_to_r[0]) / 2 * \
+        scipy.special.hankel1(nu, -k*tau_up_to_r[0]).imag
+    v_prime_0 = xx.imag
 
 
 v, v_prime = odeint(M_derivs_homo, [v_0, v_prime_0], tau).T
 
-
+'''
 ax1.plot(
     tau, v, label=r"Numerical solution", color="black"
 )
 
+if isReal:
+    ax1.plot(
+        tau_up_to_r, np.sqrt(-np.pi*tau_up_to_r) / 2 *
+        scipy.special.hankel1(nu, -k*tau_up_to_r), "--", color="orange",
+        label=r"Analyt Soln: $\frac{\sqrt{-\pi \tau}}{2} H_\nu^{(1)} (-k\tau)$"
+    )
+else:
+    ax1.plot(
+        tau_up_to_r, -1j*np.sqrt(-np.pi*tau_up_to_r) / 2 *
+        scipy.special.hankel1(nu, -k*tau_up_to_r), "--", color="orange",
+        label=r"Analyt Soln: $\frac{\sqrt{-\pi \tau}}{2} H_\nu^{(1)} (-k\tau)$"
+    )
+'''
 
-ax1.plot(
-    tau_up_to_r, -1j*np.sqrt(-np.pi*tau_up_to_r) / 2 *
-    scipy.special.hankel1(nu, -k*tau_up_to_r), "--", color="orange",
-    label=r"Analyt Soln: $\frac{\sqrt{-\pi \tau}}{2} H_\nu^{(1)} (-k\tau)$"
-)
-
-
-tau_end = 6*tau_m
-tau_rest = np.linspace(tau_r, tau_end, N)
+tau_end = 1e10*tau_m
+tau_rest = np.logspace(tau_r, tau_end, N)
 v_0_rest = v[N-1]
 xxxxx = np.array([tau_up_to_r[N-1] - 0.0000001, tau_up_to_r[N-1] + 0.0000001])
-xxxx = (np.sqrt(-np.pi*xxxxx) / 2 * scipy.special.hankel1(nu, -k*xxxxx))[:2]
+if isReal:
+    xxxx = (np.sqrt(-np.pi*xxxxx) / 2 *
+            scipy.special.hankel1(nu, -k*xxxxx))[:2]
+else:
+    xxxx = -1j*(np.sqrt(-np.pi*xxxxx) / 2 *
+                scipy.special.hankel1(nu, -k*xxxxx))[:2]
 xxxx = xxxx[1] - xxxx[0]
 xxxx /= xxxxx[1] - xxxxx[0]
 v_prime_0_rest = xxxx
@@ -109,16 +123,19 @@ ax1.set_xlabel(r"$\tau$ (conformal time)")
 
 tau_r_to_m = np.empty([0])
 for val in tau_rest:
-    if val < tau_m:
+    if val <= tau_m:
         tau_r_to_m = np.append(tau_r_to_m, val)
 
 C_1 = -1j*np.sqrt(np.pi)*2**(-7/2 + nu)*(k*tau_r)**(-nu)*scipy.special.gamma(nu)*(2*m /
                                                                                   H_inf * scipy.special.jv(-3/4, m/(2*H_inf)) + (1-2*nu)*scipy.special.jv(1/4, m/(2*H_inf)))
 C_2 = -1j*np.sqrt(np.pi)*2**(-7/2 + nu)*(k*tau_r)**(-nu)*scipy.special.gamma(nu)*(2*m /
                                                                                   H_inf * scipy.special.jv(3/4, m/(2*H_inf)) - (1-2*nu)*scipy.special.jv(-1/4, m/(2*H_inf)))
-v_k_reg2 = -1j*2/np.sqrt(np.pi*a_r * tau_r_to_m / tau_r*m)*(C_1*np.cos(a_r * tau_r_to_m / tau_r*m*tau_r_to_m/2 - np.pi/8) + C_2*np.sin(a_r * tau_r_to_m / tau_r*m*tau_r_to_m/2 + np.pi/8))
+v_k_reg2 = 2/np.sqrt(np.pi*a_r * tau_r_to_m / tau_r*m)*(C_1*np.cos(a_r * tau_r_to_m / tau_r *
+                                                                   m*tau_r_to_m/2 - np.pi/8) + C_2*np.sin(a_r * tau_r_to_m / tau_r*m*tau_r_to_m/2 + np.pi/8))
+
+
 ax1.plot(
-    tau_r_to_m, v_k_reg2 , "--", color="cyan",
+    tau_r_to_m, -1j*v_k_reg2, "--", color="cyan",
     label=r"Analyt Solution: $\frac{2}{\sqrt{\pi am}}[C_1 \cos(\frac{am\tau}{2} - \frac{\pi}{8}) + C_2 \sin(\frac{am\tau}{2} + \frac{\pi}{8})]$"
 )
 
@@ -136,15 +153,19 @@ ax1.plot(
     color="magenta",
     label=r"Analyt Solution: $\frac{2}{k}\sqrt{\frac{m\tau_m}{\pi H_{inf} \tau_r^2}}[D_1 \cos(k\tau) + D_2 \sin(k\tau)]$"
 )
+
 print(lamb)
-print(k*tau_m)
+print(a_r * tau_m / tau_r *m*tau_m/2 )
 
 ax1.set_xlim(-tau_end, tau_end)
+ax1.set_xlim(tau_r, tau_end)
 ax1.set_ylabel(r"$v_k(\tau)$")
+ax1.set_xscale('log')
 
-
+'''
 plt.axvline(x=-tau_r, color='red', linestyle='dashed',
             linewidth=1)
+ '''       
 plt.axvline(x=tau_r, color='red', linestyle='dashed',
             linewidth=1, label=r'$\pm \tau_r$')
 plt.axvline(x=tau_m, color='blue', linestyle='dashed',
@@ -182,5 +203,5 @@ plt.title(r"Comparison betw. Analytical soln. and expected lim, complex")
 '''
 
 plt.legend()
-plt.savefig("fixed-mode-func-small-k.pdf")
+# plt.savefig("fixed-mode-func-small-k.pdf")
 plt.show()
