14bbad4c2ced873f763aa5ab57345a2adc4f58b4
Chris Choi <mychoi314@gmail.com>
2023-06-23 14:59:04 -0400

    did some more tests

--- emir/emir.py
+++ emir/emir.py
@@ -17,6 +17,7 @@ omega_M = .3
 omega_R = 8.5e-5
 omega_L = .7
 eta_rm = .1
+
 eta_ml = 12.5
 K = 0
 M_pl /= hbar
@@ -41,7 +42,7 @@ k_c = 1e4*H_0
 eta_rm = .1
 a_c = k_c / M_GW
 a_0 = k_0 / M_GW
-
+eta_rm = 4*np.sqrt(omega_R)/omega_M/H_0
 
 def a_rd(eta):
     return H_0*np.sqrt(omega_R)*eta
@@ -49,14 +50,6 @@ def a_rd(eta):
 
 def a_md(eta):
     return H_0**2*.25*omega_M*eta**2
-
-
-def a_rd_p(eta):
-    return H_0*np.sqrt(omega_R)
-
-
-def a_md_p(eta):
-    return 2*eta*H_0**2*.25*omega_M
 
 
 def scale_fac(conf_time):
@@ -76,21 +69,12 @@ def d_scale_fac_dz(conf_time):
 def diffeqMG(M, u):
     r = [M[1], -((c_g*k)**2 + (scale_fac(u)*M_GW)**2 -
                  d_scale_fac_dz(u) / scale_fac(u) + 2*K*c_g**2) * M[0]]
-    # print(r)
     return r
 
 
 def diffeqGR(M, u):
     r = [M[1], -((c_g*k)**2 -
                  d_scale_fac_dz(u) / scale_fac(u) + 2*K*c_g**2) * M[0]]
-    # print(r)
-    return r
-
-
-def M_derivs_homoJ(M, u):
-    r = [M[1], -((c_g*k)**2 + (a_rd(u)*M_GW)**2 -
-                 a_rd_p(u) / a_rd(u) + 2*K*c_g**2) * M[0]]
-    print(r)
     return r
 
 
@@ -102,53 +86,52 @@ def ang_freq(conf_t):
     return np.sqrt(k**2/scale_fac(conf_t)**2 + M_GW**2)
 
 
-N = 5000
-k_arr = [1e3, 5e3, 1e4]
-'''
+def normalize(array):
+    max = array.max()
+    array /= max
+    return array
+
+def normalize_0(array):
+    max = array[0]
+    array /= max
+    return array
+
+N = 2000
+k_arr = [1e3, 5e3, 1e4,5e4,1e5,1e6,1e7]
+m_arr = [1e-5,1e-6, 1e-7, 1e-8 ]
 fig, (ax1, ax2, ax3) = plt.subplots(3, figsize=(22, 14))
 
-eta = np.logspace(-5, 0, N)
+eta = np.logspace(-10, 1 , N)
+#eta = np.logspace(-3, -1, N)
+a = np.vectorize(scale_fac)(eta)
+a = normalize_0(a)
 v_0 = 1
 v_prime_0 = 0
+k = 1e4
 for k in k_arr:
     v, v_prime = odeint(diffeqGR, [v_0, v_prime_0], eta).T
-    ax1.plot(eta, v, label=f'{k}'+r' $Gpc^{-1}$')
+    ax1.plot(eta, v, label=f'{1/gpc2hz(k)}'+r' $Hz$')
 for k in k_arr:
     v, v_prime = odeint(diffeqMG, [v_0, v_prime_0], eta).T
-    ax2.plot(eta, v, label=f'{k}'+r' $Gpc^{-1}$')
-
-k = k_c
+    ax2.plot(eta, v, label=f'{1/gpc2hz(k)}'+r' $Hz$')
+    #ax2.plot(eta, v, label=f'{M_GW*hbar}'+r' $GeV$')
+k = 10000    
 C = 4*H_0*a_0*eta_rm**(3/2) / (9*M_pl*k**(3/2))
 gamma_k = C*3/k**2*(-np.cos(k*eta)+np.sin(k*eta)/(k*eta))
-max_k = gamma_k.max()
-gamma_k /= max_k
-ax1.plot(eta, gamma_k, "--", label="Analyt soln, k = k_c")
+gamma_k = normalize(gamma_k)
+ax1.plot(eta, gamma_k/a, "--", label="Analyt soln, k = k_c")
 p = 0
 q = 2*(p+3)
 gamma_k2_approx = 2**(1/4 + 3/(2*q))*C*(3/2)**(3/2-3/q) * \
     M_GW**(-3/q)/(np.sqrt(k))*np.cos(k*eta)
-max_k2_approx = gamma_k2_approx.max()
-gamma_k2_approx /= max_k2_approx
+gamma_k2_approx = normalize(gamma_k2_approx)
 # ax2.plot(eta, gamma_k2_approx, "--", label="Analyt soln approx, k = k_c")
 gamma_k2 = C*(3/2)**(3/2-3/q)*q**(3/q)*scipy.special.gamma(1+3/q)/M_GW**(3/q) * \
     eta**(1/2)*scipy.special.jv(3/q, 3*(2/3)**(q/2)*M_GW/q*eta**(q/2))
-max_k2 = gamma_k2.max()
-gamma_k2 /= max_k2
-ax2.plot(eta, gamma_k2, "--", label="Analyt soln exact, k = k_c")
+gamma_k2 = normalize(gamma_k2)
+ax2.plot(eta, gamma_k2/a, "--", label="Analyt soln exact, k = k_c")
 
 
-H_square = np.vectorize(Hubble)(eta)**2
-ang_freq_square = np.vectorize(ang_freq)(eta)**2
-ax3.plot(eta, H_square, label=r'$H^2$'+f': {k}'+r' $Gpc^{-1}$')
-ax3.plot(eta, ang_freq_square,
-         label=r'$\omega^2$: '+f'{k}'+r' $Gpc^{-1}$')
-eta_k = 0
-k_idx = 0
-for i in range(0, len(eta)):
-    if H_square[i] <= ang_freq_square[i]:
-        eta_k = eta[i]
-        k_idx = i
-        break
 
 ax1.title.set_text(r'GR for $k \approx k_c$')
 ax1.set_ylabel(r'$\gamma_k(\eta)$')
@@ -162,13 +145,29 @@ ax2.set_ylabel(r'$\gamma_k(\eta)$')
 ax2.set_xscale('log')
 ax2.legend(loc='best')
 
-plt.axvline(x=eta_k, color="red", linestyle="dashed",
-            linewidth=1, label="Horizon crossing")
+H_square = np.vectorize(Hubble)(eta)**2
+
+for k in k_arr:
+    ang_freq_square = np.vectorize(ang_freq)(eta)**2
+    eta_k = 0
+    k_idx = 0
+    for i in range(0, len(eta)):
+        if H_square[i] <= ang_freq_square[i]:
+            eta_k = eta[i]
+            k_idx = i
+            break
+    plt.axvline(x=eta_k, linestyle="dashed",
+                linewidth=1, label="Horizon crossing: " +f'{k}'+r' $Gpc^{-1}$')
+
+ax3.plot(eta, H_square, label=r'$H^2$'+f': {k}'+r' $Gpc^{-1}$')
+ax3.plot(eta, ang_freq_square,
+         label=r'$\omega^2$: '+f'{k}'+r' $Gpc^{-1}$')
 
 ax3.title.set_text(r'Horizon Crossing')
 ax3.set_xlabel(r'$\eta(Gpc)$')
 ax3.set_xscale('log')
 ax3.legend(loc='best')
+
 '''
 
 fig, (ax4) = plt.subplots(1, figsize=(22, 14))
@@ -193,11 +192,12 @@ plt.hlines(y=k_c, xmin=1e-10, xmax=a_c, color="black", linestyle="dashed",
 ax4.text(4e-11, k_c, r"$k_c$", fontsize=15, weight="bold")
 
 ax4.set_xlim(1e-10,5e0)
-ax4.set_ylim(1e0, 5e10 )
+ax4.set_ylim(1e0, 5e10)
 ax4.set_xscale('log')
 ax4.set_yscale('log')
 ax4.legend(loc='best')
 
+'''
 
-plt.savefig("emir/fig3.pdf")
+plt.savefig("emir/fig5.pdf")
 plt.show()
