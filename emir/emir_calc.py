0c98b7a87dcb370cc40bdace45ff6b0b22160440
Chris Choi <mychoi314@gmail.com>
2023-06-28 13:10:34 -0400

    Trying to figure out the reason the transfer func doesnt look right

--- emir/emir_calc.py
+++ emir/emir_calc.py
@@ -5,8 +5,11 @@ from scipy.integrate import odeint
 from emir_func import *
 
 N = 2000
-k = k_0
-eta = np.logspace(-7,1, N)
+omega_0 = k_0/a_0
+k = a_0*np.sqrt(omega_0**2-M_GW**2)
+print(K)
+k = 1e1
+eta = np.logspace(-10, 2, N)
 H_square = np.vectorize(Hubble)(eta) ** 2
 ang_freq_square = np.vectorize(ang_freq)(eta) ** 2
 a = np.vectorize(scale_fac)(eta)
@@ -18,26 +21,43 @@ for i in range(0, len(eta)):
         k_idx = i
         break
 
-print("Current eta_0 = "+f'{eta[k_idx]}')
+# print("Current eta_0 = "+f'{eta[k_idx]}')
 
-time = scipy.integrate.cumtrapz(a,eta,initial=0)
+time = scipy.integrate.cumtrapz(a, eta, initial=0)
 
 fig, (ax1) = plt.subplots(1)
 
-ax1.plot(eta, a, label = 'a')
-ax1.axhline(y = (k/M_GW)**2, linestyle="dashed",
-           linewidth=1, label=r'$k_0/M_{GW}$')
-ax1.plot(eta, time, label = 'Physical time')
-res, err = scipy.integrate.quad(scale_fac, 0, 2.105)
+#ax1.plot(eta, a, label='a')
+#ax1.axhline(y=(k/M_GW)**2, linestyle="dashed",
+#            linewidth=1, label=r'$k_0/M_{GW}$')
+#ax1.plot(eta, time, label='Physical time')
+#res, err = scipy.integrate.quad(scale_fac, 0, 2.105)
 
-print('Time: ' + f'{res}')
+# print('Time: ' + f'{res}')
 
-roots = scipy.optimize.fsolve(lambda x: scipy.integrate.quad(scale_fac, 0, x)[0] - 1 , 1)
-print('Roots: ', roots[0])
+#roots = scipy.optimize.fsolve(
+#    lambda x: scipy.integrate.quad(scale_fac, 0, x)[0] - 1, 1)
+# print('Roots: ', roots[0])
+
+
+
+a_k_1 = omega_M*H_0**2+H_0*np.sqrt(4*omega_R*k**2+(H_0*omega_M)**2)/(2*k**2)
+print('Using inverse with Wolfalph: ', a_k_1)
+idx_1 = 0
+H_reg = np.vectorize(Hubble)(eta)
+for i in range(0, len(eta)):
+    if k >= a[i]*H_reg[i]:
+        idx_1 = i
+        break
+print('Using graph: ', a[idx_1])
+
+ax1.plot(a, a*H_reg,label='aH', color='orange')
+ax1.axhline(y=k, label='k')
 
 ax1.legend(loc='best')
-#ax1.set_xscale('log')
+ax1.set_xscale('log')
+ax1.set_yscale('log')
 ax1.set_title('Scale Factor')
 
-#plt.savefig("emir/emir_calc_figs/scale_factor_evolution.pdf")
-#plt.show()
+# plt.savefig("emir/emir_calc_figs/scale_factor_evolution.pdf")
+plt.show()
--- emir/emir_power.py
+++ emir/emir_power.py
@@ -1,13 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy
+import math
 from scipy.integrate import odeint
 from emir_func import *
 
-N = 2000
+N = 100
 eta = np.logspace(-7,1,N)
 #k = np.logspace(-5,5, N)
 omega_0 = k_0/a_0
+omega_0 = np.logspace(math.log(M_GW,10)+.00001,math.log(M_GW,10) + 10, N)
 k_prime = a_0*omega_0 # seems a bit circular, but that is how Mukohyama defined it in his paper
 eq_idx = 0
 H_square = np.vectorize(Hubble)(eta)**2
@@ -17,11 +19,26 @@ for i in range(0, len(eta)):
         eta_k = eta[i]
         eq_idx = i
         break
+
+
 a_eq = scale_fac(eta_rm)
 H_eq = Hubble(eta_rm)
 
 k = a_0*np.sqrt(omega_0**2-M_GW**2)
-omega_k = ang_freq((k/a_0)**2 + M_GW**2)
+omega_k = np.sqrt((k/a_0)**2 + M_GW**2)
+print(omega_k)
+print(omega_k**2 - M_GW**2)
 a_k = k / np.sqrt(omega_k**2 - M_GW**2)
-a_k_prime_GR = k_prime/ang_freq_GR(eta_rm)    
+beta = H_eq**2*a_eq**4/(2)
+a_k_prime_GR = (beta+np.sqrt(beta)*np.sqrt(4*a_eq**2*k_prime**2+beta))/(2*a_eq*k_prime**2)
 S = k_prime* a_k / (k* a_k_prime_GR)*np.sqrt(omega_k*a_k/(omega_0*a_0))
+
+fig, (ax1) = plt.subplots(1)
+ax1.plot(omega_0, S, label = r'$S(\omega_0)$')
+
+ax1.legend(loc='best')
+ax1.set_xscale('log')
+ax1.set_title('Transfer Function')
+
+plt.savefig("emir/emir_power_figs/fig1.pdf")
+plt.show()
