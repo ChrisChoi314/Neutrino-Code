e14b7c7be79779e5630eef434db703e16d972732
Chris Choi <mychoi314@gmail.com>
2024-04-02 11:03:42 -0400

    Successfully debugged the issue of the wrong placement of the orange thingies

--- nanograv/2d.py
+++ nanograv/2d.py
@@ -29,8 +29,6 @@ freqs_30 = f
 def omega_GW(f, A_cp, gamma):
     return 2*np.pi**2*(10**A_cp)**2*f_yr**2/(3*H_0**2)*(f/f_yr)**(5-gamma)
 
-#plt.figure(figsize=(10,4))
-
 
 dir = '30f_fs{hd}_ceffyl'
 freqs = np.load('blue/data/NANOGrav15yr_KDE-FreeSpectra_v1.0.0/' + dir + '/freqs.npy')
@@ -71,31 +69,30 @@ tau_m = 1e27*tau_r
 freqs = np.logspace(-19,np.log10(f_UV),num_freqs)
 
 M_arr = np.linspace(0.00000001, 1.499999, N)
-#M_arr = np.logspace(-6,np.log10(1.499999), N)
 colors = ['black', 'black']
 lw = 2
 thicker = 1.5
 
 omega_arr = [omega_GW_full(freqs_boundary[0], M_arr*H_inf, H_inf, tau_r, tau_m), omega_GW_full(freqs_boundary[1], M_arr*H_inf, H_inf, tau_r, tau_m)]
-partition_idx_low = [0,0,0,0,0,0]
-partition_idx_high = [0,0,0,0,0,0]
-for i in range(len(omega_arr[0])):
+last_idx = len(omega_arr[0])-1
+partition_idx_low = [last_idx,last_idx,last_idx,last_idx,last_idx,last_idx]
+partition_idx_high = [last_idx,last_idx,last_idx,last_idx,last_idx,last_idx]
+i = last_idx
+while i >= 0:
     for j in range(6):
         if omega_arr[0][i] <= Omega_sig_f_low[j]:
-            partition_idx_low[j] += 1
+            partition_idx_low[j] -= 1
         if omega_arr[1][i] <= Omega_sig_f_high[j]:
-            partition_idx_high[j] += 1
+            partition_idx_high[j] -= 1
+    i-=1
 
 ax1.plot(M_arr, omega_arr[0], color = colors[0], linewidth = lw)
 ax1.plot(M_arr, omega_arr[1], color = colors[1], linewidth = lw, linestyle='dashed')
 
 for i in range(3):
-    ax1.plot(M_arr[partition_idx_low[2*i]:partition_idx_low[2*i+1]], omega_arr[0][partition_idx_low[2*i]:partition_idx_low[2*i+1]] , color = 'orange', alpha = 0.7-i*0.2, linewidth = lw+thicker)
-    ax1.plot(M_arr[partition_idx_low[2*i]:partition_idx_low[2*i+1]], omega_arr[1][partition_idx_low[2*i]:partition_idx_low[2*i+1]] , color = 'orange', alpha = 0.7-i*0.2, linewidth = lw+thicker)
+    ax1.plot(M_arr[partition_idx_low[2*i+1]:partition_idx_low[2*i]], omega_arr[0][partition_idx_low[2*i+1]:partition_idx_low[2*i]] , color = 'orange', alpha = 0.7-i*0.2, linewidth = lw+thicker)
+    ax1.plot(M_arr[partition_idx_low[2*i+1]:partition_idx_low[2*i]], omega_arr[1][partition_idx_low[2*i+1]:partition_idx_low[2*i]] , color = 'orange', alpha = 0.7-i*0.2, linewidth = lw+thicker)
 
-#ax1.plot(M_arr[omega_arr[0] < Omega_sig_f_low[1] and omega_arr[0] > Omega_sig_f_low[0]], omega_arr[0][(omega_arr[0] < Omega_sig_f_low[1] and omega_arr[0] > Omega_sig_f_low[0])], alpha=0.7, color= 'orange')
-
-#ax1.set_xscale('log')
 ax1.set_yscale('log')
 ax1.set_xlabel(r'$m/H_{\mathrm{inf}}$')
 ax1.set_xlim(0,1.5)
@@ -141,21 +138,23 @@ tau_m = 1e27*tau_r
 M_GW = H_inf
 
 omega_arr = [omega_GW_full(freqs_boundary[0],  M_GW, H_inf, tau_r, tau_m), omega_GW_full(freqs_boundary[1], M_GW, H_inf, tau_r, tau_m)]
-partition_idx_low = [0,0,0,0,0,0]
-partition_idx_high = [0,0,0,0,0,0]
-for i in range(len(omega_arr[0])):
+partition_idx_low = [last_idx,last_idx,last_idx,last_idx,last_idx,last_idx]
+partition_idx_high = [last_idx,last_idx,last_idx,last_idx,last_idx,last_idx]
+i = last_idx
+while i >= 0:
     for j in range(6):
         if omega_arr[0][i] <= Omega_sig_f_low[j]:
-            partition_idx_low[j] += 1
+            partition_idx_low[j] -= 1
         if omega_arr[1][i] <= Omega_sig_f_high[j]:
-            partition_idx_high[j] += 1
+            partition_idx_high[j] -= 1
+    i-=1
 
 ax3.plot(H_inf, omega_GW_full(freqs_boundary[0], M_GW, H_inf, tau_r, tau_m), color = colors[0], linewidth = lw)
 ax3.plot(H_inf, omega_GW_full(freqs_boundary[1], M_GW, H_inf, tau_r, tau_m), color = colors[1], linewidth = lw, linestyle='dashed')
 
 for i in range(3):
-    ax3.plot(H_inf[partition_idx_low[2*i]:partition_idx_low[2*i+1]], omega_arr[0][partition_idx_low[2*i]:partition_idx_low[2*i+1]] , color = 'orange', alpha = 0.7-i*0.2, linewidth = lw+thicker)
-    ax3.plot(H_inf  [partition_idx_low[2*i]:partition_idx_low[2*i+1]], omega_arr[1][partition_idx_low[2*i]:partition_idx_low[2*i+1]] , color = 'orange', alpha = 0.7-i*0.2, linewidth = lw+thicker)
+    ax3.plot(H_inf[partition_idx_low[2*i+1]:partition_idx_low[2*i]], omega_arr[0][partition_idx_low[2*i+1]:partition_idx_low[2*i]] , color = 'orange', alpha = 0.7-i*0.2, linewidth = lw+thicker)   
+    ax3.plot(H_inf  [partition_idx_low[2*i+1]:partition_idx_low[2*i]], omega_arr[1][partition_idx_low[2*i+1]:partition_idx_low[2*i]] , color = 'orange', alpha = 0.7-i*0.2, linewidth = lw+thicker)
 
 
 ax3.set_xscale('log')
@@ -164,9 +163,9 @@ ax3.set_xlabel(r'$H_{\mathrm{inf}}$ [GeV]')
 ax3.set_xlim(1e-20,1e10)
 ax3.set_ylim(1e-18,1e7)
 
-ax3.annotate(r'$m = H_{\mathrm{inf}}, \tau_m = 10^{27}\tau_r$', xy=(0.05,0.15),xycoords='axes fraction',fontsize=fs)
+ax3.annotate(r'$m = H_{\mathrm{inf}}, \tau_m/\tau_r = 10^{27}$', xy=(0.05,0.15),xycoords='axes fraction',fontsize=fs)
 ax3.annotate(r'$f_{\mathrm{low}}$', xy=(0.5,0.35),xycoords='axes fraction',fontsize=fs)
-ax3.annotate(r'$f_{\mathrm{high}}$', xy=(0.55,0.52),xycoords='axes fraction',fontsize=fs)
+ax3.annotate(r'$f_{\mathrm{high}}$', xy=(0.55,0.5),xycoords='axes fraction',fontsize=fs)
 
 axs = [ax1,ax2,ax3]
 
@@ -174,79 +173,6 @@ for i in range(3):
     axs[i].set_ylabel(r'$h_0^2\Omega_{\mathrm{GW}}$')
     axs[i].grid(which='major', alpha=.2)
     axs[i].grid(which='minor', alpha=.2)
-# for the second figure 
-'''
-plt.gca()
-ax.clear()
-
-tau_m_arr = np.logspace(0,np.log10(1e10*(H_inf/1e14)**-2), N)
-
-M_GW = H_inf
-
-def f(x, y):
-    return omega_GW_full(x, M_GW, H_inf, tau_r, y)
-
-X, Y = np.meshgrid(freqs, tau_m_arr*tau_r)
-Z = f(X, Y)
-
-plt.figure(figsize=(10,8))
-ax = plt.axes(projection='3d')
-surf = ax.plot_surface( np.log10(X),np.log10(Y/tau_r), np.log10(Z),linestyles="solid",)
-ax.set_xlabel('f [Hz]')
-ax.set_ylabel(r'$\tau_m \ [\tau_r]$')
-ax.set_zlabel('$h_0^2\Omega_{\mathrm{GW}}$')
-
-ax.xaxis.set_major_formatter(mticker.FuncFormatter(log_tick_formatter))
-ax.xaxis.set_major_locator(mticker.MaxNLocator(integer=True))
-ax.yaxis.set_major_formatter(mticker.FuncFormatter(log_tick_formatter))
-ax.yaxis.set_major_locator(mticker.MaxNLocator(integer=True))
-ax.zaxis.set_major_formatter(mticker.FuncFormatter(log_tick_formatter))
-ax.zaxis.set_major_locator(mticker.MaxNLocator(integer=True))
-
-#ax.view_init(elev=20, azim=120)
-ax.annotate(r'$m = H_{\mathrm{inf}}, H_{\mathrm{inf}} = 10^8$ GeV', xy=(0.4,0.9),xycoords='axes fraction',
-             fontsize=fs)
-
-plt.savefig('nanograv/contour_figs/fig4b.pdf')
-
-# for the third figure 
-
-plt.gca()
-ax.clear()
-
-
-H_inf = np.logspace(-24,14, N) # in GeV
-
-tau_r = 1/(a_r*H_inf)
-
-tau_m = 1e21*tau_r 
-M_GW = H_inf
-
-def f(x, y):
-    return omega_GW_full(x, M_GW, y, tau_r, tau_m)
-
-X, Y = np.meshgrid(freqs, H_inf)
-Z = f(X, Y)
-
-plt.figure(figsize=(10,8))
-ax = plt.axes(projection='3d')
-surf = ax.plot_surface( np.log10(X),np.log10(Y), np.log10(Z),linestyles="solid")
-ax.set_xlabel('f [Hz]')
-ax.set_ylabel(r'$H_{\mathrm{inf}}$')
-ax.set_zlabel('$h_0^2\Omega_{\mathrm{GW}}$')
-
-ax.xaxis.set_major_formatter(mticker.FuncFormatter(log_tick_formatter))
-ax.xaxis.set_major_locator(mticker.MaxNLocator(integer=True))
-ax.yaxis.set_major_formatter(mticker.FuncFormatter(log_tick_formatter))
-ax.yaxis.set_major_locator(mticker.MaxNLocator(integer=True))
-ax.zaxis.set_major_formatter(mticker.FuncFormatter(log_tick_formatter))
-ax.zaxis.set_major_locator(mticker.MaxNLocator(integer=True))
-
-
-#ax.view_init(elev=20, azim=120)
-ax.annotate(r'$m = H_{\mathrm{inf}}, \tau_m = 10^{21}\tau_r$', xy=(0.4,0.9),xycoords='axes fraction',
-             fontsize=fs)
-
-'''
+    
 plt.savefig('nanograv/2d_figs/fig0.pdf')
 plt.show()
