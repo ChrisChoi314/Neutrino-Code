76ff470fa1834acd8605b9afb4243b05bd4afe36
Chris Choi <mychoi314@gmail.com>
2024-04-01 21:45:12 -0400

    Now have a 2d figure that i can add

--- nanograv/2d.py
+++ nanograv/2d.py
@@ -56,8 +56,9 @@ Omega_sig_f_high += [10**(OMG_15.mean(axis=0) - 1*OMG_15.std(axis=0))[-1], 10**(
 Omega_sig_f_high += [10**(OMG_15.mean(axis=0) - 2*OMG_15.std(axis=0))[-1], 10**(OMG_15.mean(axis=0) + 2*OMG_15.std(axis=0))[-1]]
 Omega_sig_f_high += [10**(OMG_15.mean(axis=0) - 3*OMG_15.std(axis=0))[-1], 10**(OMG_15.mean(axis=0) + 3*OMG_15.std(axis=0))[-1]]
 
-fig, (ax1,ax2,ax3) = plt.subplots(3, 1, figsize=(8,12))
+fig, (ax1,ax2,ax3) = plt.subplots(3, 1, figsize=(6,10),constrained_layout = True)
 
+ 
 N = 2000
 num_freqs = N
 H_inf = 1  # in GeV
@@ -68,46 +69,104 @@ f_UV = 1/tau_r / (2*np.pi)
 tau_m = 1e27*tau_r
 
 freqs = np.logspace(-19,np.log10(f_UV),num_freqs)
- 
+
 M_arr = np.linspace(0.00000001, 1.499999, N)
 #M_arr = np.logspace(-6,np.log10(1.499999), N)
-colors = ['black', 'red']
+colors = ['black', 'black']
+lw = 2
+thicker = 1.5
 
-ax1.plot(M_arr, omega_GW_full(freqs_boundary[0], M_arr*H_inf, H_inf, tau_r, tau_m), color = colors[0],linewidth = 2)
-ax1.plot(M_arr, omega_GW_full(freqs_boundary[1], M_arr*H_inf, H_inf, tau_r, tau_m), color = colors[1])
+omega_arr = [omega_GW_full(freqs_boundary[0], M_arr*H_inf, H_inf, tau_r, tau_m), omega_GW_full(freqs_boundary[1], M_arr*H_inf, H_inf, tau_r, tau_m)]
+partition_idx_low = [0,0,0,0,0,0]
+partition_idx_high = [0,0,0,0,0,0]
+for i in range(len(omega_arr[0])):
+    for j in range(6):
+        if omega_arr[0][i] <= Omega_sig_f_low[j]:
+            partition_idx_low[j] += 1
+        if omega_arr[1][i] <= Omega_sig_f_high[j]:
+            partition_idx_high[j] += 1
 
+ax1.plot(M_arr, omega_arr[0], color = colors[0], linewidth = lw)
+ax1.plot(M_arr, omega_arr[1], color = colors[1], linewidth = lw, linestyle='dashed')
+
+for i in range(3):
+    ax1.plot(M_arr[partition_idx_low[2*i]:partition_idx_low[2*i+1]], omega_arr[0][partition_idx_low[2*i]:partition_idx_low[2*i+1]] , color = 'orange', alpha = 0.7-i*0.2, linewidth = lw+thicker)
+    ax1.plot(M_arr[partition_idx_low[2*i]:partition_idx_low[2*i+1]], omega_arr[1][partition_idx_low[2*i]:partition_idx_low[2*i+1]] , color = 'orange', alpha = 0.7-i*0.2, linewidth = lw+thicker)
+
+#ax1.plot(M_arr[omega_arr[0] < Omega_sig_f_low[1] and omega_arr[0] > Omega_sig_f_low[0]], omega_arr[0][(omega_arr[0] < Omega_sig_f_low[1] and omega_arr[0] > Omega_sig_f_low[0])], alpha=0.7, color= 'orange')
 
 #ax1.set_xscale('log')
 ax1.set_yscale('log')
-ax1.set_xlabel(r'$m$')
+ax1.set_xlabel(r'$m/H_{\mathrm{inf}}$')
 ax1.set_xlim(0,1.5)
 
-#ax1.annotate(r'$\tau_m = 10^{21}\tau_r, H_{\mathrm{inf}} = 10^8$ GeV', xy=(0.4,0.9),xycoords='axes fraction',fontsize=fs)
+ax1.annotate(r'$\tau_m/\tau_r = 10^{27}, H_{\mathrm{inf}} = 1$ GeV', xy=(0.05,0.15),xycoords='axes fraction',fontsize=fs)
+ax1.annotate(r'$f_{\mathrm{low}}$', xy=(0.77,0.5),xycoords='axes fraction',fontsize=fs)
+ax1.annotate(r'$f_{\mathrm{high}}$', xy=(0.85,0.60),xycoords='axes fraction',fontsize=fs)
 
-tau_m_arr = np.logspace(5,30, N)
-
+tau_m_arr = np.logspace(5,31, N)
 M_GW = H_inf
 
-ax2.plot(tau_m_arr, omega_GW_full(freqs_boundary[0], M_GW, H_inf, tau_r, tau_m_arr*tau_r), color = colors[0])
-ax2.plot(tau_m_arr, omega_GW_full(freqs_boundary[1], M_GW, H_inf, tau_r, tau_m_arr*tau_r), color = colors[1])
+omega_arr = [omega_GW_full(freqs_boundary[0],  M_GW, H_inf, tau_r, tau_m_arr*tau_r), omega_GW_full(freqs_boundary[1], M_GW, H_inf, tau_r, tau_m_arr*tau_r)]
+partition_idx_low = [0,0,0,0,0,0]
+partition_idx_high = [0,0,0,0,0,0]
+for i in range(len(omega_arr[0])):
+    for j in range(6):
+        if omega_arr[0][i] <= Omega_sig_f_low[j]:
+            partition_idx_low[j] += 1
+        if omega_arr[1][i] <= Omega_sig_f_high[j]:
+            partition_idx_high[j] += 1
+ax2.plot(tau_m_arr, omega_GW_full(freqs_boundary[0], M_GW, H_inf, tau_r, tau_m_arr*tau_r), color = colors[0], linewidth = lw)
+ax2.plot(tau_m_arr, omega_GW_full(freqs_boundary[1], M_GW, H_inf, tau_r, tau_m_arr*tau_r), color = colors[1], linewidth = lw, linestyle='dashed')
+
+for i in range(3):
+    ax2.plot(tau_m_arr[partition_idx_low[2*i]:partition_idx_low[2*i+1]], omega_arr[0][partition_idx_low[2*i]:partition_idx_low[2*i+1]] , color = 'orange', alpha = 0.7-i*0.2, linewidth = lw+thicker)
+    ax2.plot(tau_m_arr[partition_idx_low[2*i]:partition_idx_low[2*i+1]], omega_arr[1][partition_idx_low[2*i]:partition_idx_low[2*i+1]] , color = 'orange', alpha = 0.7-i*0.2, linewidth = lw+thicker)
 
 ax2.set_xscale('log')
 ax2.set_yscale('log')
 ax2.set_xlabel(r'$\tau_m / \tau_r$')
+ax2.set_xlim(1e10,1e30)
+ax2.set_ylim(1e-29,1e-5)
 
-H_inf = np.logspace(-24,14, N) # in GeV
+ax2.annotate(r'$m = H_{\mathrm{inf}}, H_{\mathrm{inf}} = 1$ GeV', xy=(0.05,0.85),xycoords='axes fraction',fontsize=fs)
+ax2.annotate(r'$f_{\mathrm{low}}$', xy=(0.75,0.6),xycoords='axes fraction',fontsize=fs)
+ax2.annotate(r'$f_{\mathrm{high}}$', xy=(0.7,0.8),xycoords='axes fraction',fontsize=fs)
+
+H_inf = np.logspace(-24, 14, N) # in GeV
 
 tau_r = 1/(a_r*H_inf)
 
 tau_m = 1e27*tau_r 
 M_GW = H_inf
 
-ax3.plot(H_inf, omega_GW_full(freqs_boundary[0], M_GW, H_inf, tau_r, tau_m), color = colors[0])
-ax3.plot(H_inf, omega_GW_full(freqs_boundary[1], M_GW, H_inf, tau_r, tau_m), color = colors[1])
+omega_arr = [omega_GW_full(freqs_boundary[0],  M_GW, H_inf, tau_r, tau_m), omega_GW_full(freqs_boundary[1], M_GW, H_inf, tau_r, tau_m)]
+partition_idx_low = [0,0,0,0,0,0]
+partition_idx_high = [0,0,0,0,0,0]
+for i in range(len(omega_arr[0])):
+    for j in range(6):
+        if omega_arr[0][i] <= Omega_sig_f_low[j]:
+            partition_idx_low[j] += 1
+        if omega_arr[1][i] <= Omega_sig_f_high[j]:
+            partition_idx_high[j] += 1
+
+ax3.plot(H_inf, omega_GW_full(freqs_boundary[0], M_GW, H_inf, tau_r, tau_m), color = colors[0], linewidth = lw)
+ax3.plot(H_inf, omega_GW_full(freqs_boundary[1], M_GW, H_inf, tau_r, tau_m), color = colors[1], linewidth = lw, linestyle='dashed')
+
+for i in range(3):
+    ax3.plot(H_inf[partition_idx_low[2*i]:partition_idx_low[2*i+1]], omega_arr[0][partition_idx_low[2*i]:partition_idx_low[2*i+1]] , color = 'orange', alpha = 0.7-i*0.2, linewidth = lw+thicker)
+    ax3.plot(H_inf  [partition_idx_low[2*i]:partition_idx_low[2*i+1]], omega_arr[1][partition_idx_low[2*i]:partition_idx_low[2*i+1]] , color = 'orange', alpha = 0.7-i*0.2, linewidth = lw+thicker)
+
 
 ax3.set_xscale('log')
 ax3.set_yscale('log')
-ax3.set_xlabel(r'$H_{\mathrm{inf}}$')
+ax3.set_xlabel(r'$H_{\mathrm{inf}}$ [GeV]')
+ax3.set_xlim(1e-20,1e10)
+ax3.set_ylim(1e-18,1e7)
+
+ax3.annotate(r'$m = H_{\mathrm{inf}}, \tau_m = 10^{27}\tau_r$', xy=(0.05,0.15),xycoords='axes fraction',fontsize=fs)
+ax3.annotate(r'$f_{\mathrm{low}}$', xy=(0.5,0.35),xycoords='axes fraction',fontsize=fs)
+ax3.annotate(r'$f_{\mathrm{high}}$', xy=(0.55,0.52),xycoords='axes fraction',fontsize=fs)
 
 axs = [ax1,ax2,ax3]
 
@@ -115,7 +174,6 @@ for i in range(3):
     axs[i].set_ylabel(r'$h_0^2\Omega_{\mathrm{GW}}$')
     axs[i].grid(which='major', alpha=.2)
     axs[i].grid(which='minor', alpha=.2)
-    axs[i].legend(loc='upper left')
 # for the second figure 
 '''
 plt.gca()
